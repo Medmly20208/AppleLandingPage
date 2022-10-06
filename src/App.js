e7f1e1255d27e9a502d000776c3f35f5fdb15db1
Medmly20208 <moulaymohamed856@gmail.com>
2022-10-06 21:55:53 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Page from "./components/Page/Page";
-import Table from "muicolumnhidetable";
 
 function App() {
-  const streams = [];
-
-  for (let i = 1; i < 30; i++) {
-    streams.push({
-      Temperature: Math.floor(Math.random() * 20),
-      Humidity: Math.floor(Math.random() * 20),
-      Co2: Math.floor(Math.random() * 20),
-      Pressure: Math.floor(Math.random() * 20),
-      Pressure2: Math.floor(Math.random() * 20),
-      Pressure3: Math.floor(Math.random() * 20),
-      Pressure5: Math.floor(Math.random() * 20),
-    });
-  }
   return (
     <>
-      <Table streams={streams} />
       <Page></Page>
     </>
   );
